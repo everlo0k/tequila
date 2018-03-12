f8994ad56cc68694f796f25ad02ed0ee660bf905
shenyang <everlook@126.com>
2018-03-12 17:56:14 +0800

    String reverse

---
+++ src/main/java/cn/bigears/base/string/StringReverse.java
@@ -0,0 +1,35 @@
+package cn.bigears.base.string;
+
+/**
+ * StringReverse
+ *
+ * @author shenyang
+ */
+public class StringReverse {
+
+    public static String reverse1(String s) {
+        if (s == null) {
+            return null;
+        }
+        StringBuffer stringBuffer = new StringBuffer(s);
+        return stringBuffer.reverse().toString();
+    }
+
+    public static String reverse2(String s) {
+        if (s == null) {
+            return null;
+        }
+        int length = s.length();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = length - 1; i >= 0; i--) {
+            stringBuilder.append(s.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("s:" + StringReverse.reverse1(" abcde f"));
+        System.out.println("s:" + StringReverse.reverse2(" abcde f"));
+    }
+
+}
