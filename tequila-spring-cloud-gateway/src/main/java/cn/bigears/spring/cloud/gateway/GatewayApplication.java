8ca878912ade7e2182a270fc9447bfa6ce3671bd
shenyang <shenyang@ebnew.com>
2025-11-14 16:29:53 +0800

    feat: 初始化 Spring Cloud Gateway 模块

---
+++ tequila-spring-cloud-gateway/src/main/java/cn/bigears/spring/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package cn.bigears.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GatewayApplication
+ * @author shenyang
+ * @date 2025-11-14
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+
+}
